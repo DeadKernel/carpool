47bc4ef53051f9d39ef7abcca00a3e666982dea9
Toshal Agrawal <toshalagrawal38@gmail.com>
2018-10-07 19:17:34 +0530

    booking a ride without disprides

--- carpool/transaction.py
+++ carpool/transaction.py
@@ -8,6 +8,7 @@ from flask import (
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime, timedelta
 from datetime import datetime as dt
+from math import ceil
 
 bp = Blueprint('afterbookride', __name__, url_prefix='/bookride')
 
@@ -21,92 +22,119 @@ def showRides(passroute):
     base = 'https://maps.googleapis.com/maps/api/directions/json?'
     displayrides = []
     api_key ='AIzaSyDsDBCYF8CBpzPI4kE7PxqtuQBoLVv2Crc'
+     # request to calculate cost
+    requestCost = base + urllib.parse.urlencode({'origin':originPassenger, 'destination':destinationPassenger,'key':api_key})
+    responseCost= urllib.request.urlopen(requestCost).read()
+    directionCost = json.loads(responseCost)
+    routesCost = directionCost['routes']
+    legsCost = routesCost[0]['legs']
+    distanceCost = []
+    for index in range(len(legsCost)):
+        distanceCost.append(legsCost[index]['distance']['value'])
+    total_distance_cost = float(sum(distanceCost))/1000
+    totalCost = 15
+    if total_distance_cost>1:
+        totalCost= (total_distance_cost-1)*5
+
     for document in rides.find({"End":destinationPassenger}):
-                a = dt.strptime(document['Time'],"%m/%d/%Y %I:%M %p")
-                b = dt.strptime(passroute['Time'], "%m/%d/%Y  %I:%M %p")
-                if a>b:
-                        continue
-                originDriver = document['Start']
-                destinationDriver =document['End']
-                originalWaypoints=document['waypoints']
-                print("Toshal")
-                requestChecker = base + urllib.parse.urlencode({'origin':originDriver, 'destination':originPassenger, 'waypoints':originalWaypoints,'key':api_key})
-                responseCheck= urllib.request.urlopen(requestChecker).read()
-                directionCheck = json.loads(responseCheck)
-                routesCheck = directionCheck['routes']
-                legsCheck = routesCheck[0]['legs']
-                distanceCheck = []
-                durationCheck = []
-                for index in range(len(legsCheck)):
-                    distanceCheck.append(legsCheck[index]['distance']['value'])
-                    durationCheck.append(legsCheck[index]['duration']['value'])
-                total_distance_check = float(sum(distanceCheck))/1000
-                total_time_check = timedelta(seconds=sum(durationCheck))
-                startDriver = a
-                startPassenger = b
-                print("Toshal")
-                if startDriver+total_time_check > startPassenger:
-                    continue
-                else:
-                    addedWaypoints =originalWaypoints+'|'+originPassenger
-                    requestDriver = base + urllib.parse.urlencode({'origin':originDriver, 'destination':destinationDriver, 'waypoints':originalWaypoints,'key':api_key})
-                    requestTotal = base + urllib.parse.urlencode({'origin':originDriver, 'destination':destinationDriver, 'waypoints':addedWaypoints, 'key':api_key})
-                    responseOne= urllib.request.urlopen(requestDriver).read()
-                    responseTwo = urllib.request.urlopen(requestTotal).read()
+        a = dt.strptime(document['Time'],"%m/%d/%Y %I:%M %p")
+        b = dt.strptime(passroute['Time'], "%m/%d/%Y  %I:%M %p")
+        if a>b:
+                continue
+        originDriver = document['Start']
+        destinationDriver =document['End']
+        originalWaypoints=document['waypoints']
+        print("Toshal")
+        requestChecker = base + urllib.parse.urlencode({'origin':originDriver, 'destination':originPassenger, 'waypoints':originalWaypoints,'key':api_key})
+        responseCheck= urllib.request.urlopen(requestChecker).read()
+        directionCheck = json.loads(responseCheck)
+        routesCheck = directionCheck['routes']
+        legsCheck = routesCheck[0]['legs']
+        distanceCheck = []
+        durationCheck = []
+        for index in range(len(legsCheck)):
+            distanceCheck.append(legsCheck[index]['distance']['value'])
+            durationCheck.append(legsCheck[index]['duration']['value'])
+        total_distance_check = float(sum(distanceCheck))/1000
+        total_time_check = timedelta(seconds=sum(durationCheck))
+        startDriver = a
+        startPassenger = b
+        print("Toshal")
+        if startDriver+total_time_check > startPassenger:
+            continue
+        else:
+            addedWaypoints =originalWaypoints+'|'+originPassenger
+            requestDriver = base + urllib.parse.urlencode({'origin':originDriver, 'destination':destinationDriver, 'waypoints':originalWaypoints,'key':api_key})
+            requestTotal = base + urllib.parse.urlencode({'origin':originDriver, 'destination':destinationDriver, 'waypoints':addedWaypoints, 'key':api_key})
+            responseOne= urllib.request.urlopen(requestDriver).read()
+            responseTwo = urllib.request.urlopen(requestTotal).read()
 
-                    directionOne = json.loads(responseOne)
-                    directionTwo = json.loads(responseTwo)
+            directionOne = json.loads(responseOne)
+            directionTwo = json.loads(responseTwo)
 
-                    #print(directionOne)
-                    #print(directionTwo)
+            #print(directionOne)
+            #print(directionTwo)
 
-                    routesOne = directionOne['routes']
-                    legsOne = routesOne[0]['legs']
-                    distanceOne = []
-                    durationOne = []
-                    for index in range(len(legsOne)):
-                        distanceOne.append(legsOne[index]['distance']['value'])
-                        durationOne.append(legsOne[index]['duration']['value'])
+            routesOne = directionOne['routes']
+            legsOne = routesOne[0]['legs']
+            distanceOne = []
+            durationOne = []
+            for index in range(len(legsOne)):
+                distanceOne.append(legsOne[index]['distance']['value'])
+                durationOne.append(legsOne[index]['duration']['value'])
 
-                    total_distance_one = float(sum(distanceOne))/1000
-                    total_distance_one =round(total_distance_one,1)
-                    total_time_one = timedelta(seconds=sum(durationOne))
-                    totalDurationOne = sum(durationOne)
-                    print(total_distance_one,'km')
-                    print(str(total_time_one))
+            total_distance_one = float(sum(distanceOne))/1000
+            total_distance_one =round(total_distance_one,1)
+            total_time_one = timedelta(seconds=sum(durationOne))
+            totalDurationOne = sum(durationOne)
+            print(total_distance_one,'km')
+            print(str(total_time_one))
 
-                    routesTwo = directionTwo['routes']
-                    legsTwo = routesTwo[0]['legs']
-                    distanceTwo = []
-                    durationTwo = []
-                    for index in range(len(legsTwo)):
-                        distanceTwo.append(legsTwo[index]['distance']['value'])
-                        durationTwo.append(legsTwo[index]['duration']['value'])
+            routesTwo = directionTwo['routes']
+            legsTwo = routesTwo[0]['legs']
+            distanceTwo = []
+            durationTwo = []
+            for index in range(len(legsTwo)):
+                distanceTwo.append(legsTwo[index]['distance']['value'])
+                durationTwo.append(legsTwo[index]['duration']['value'])
 
-                    total_distance_two = float(sum(distanceTwo))/1000
-                    total_distance_two =round(total_distance_two,1)
-                    total_time_two = timedelta(seconds=sum(durationTwo))
-                    totalDurationTwo= sum(durationTwo)
-                    print(total_distance_two,'km')
-                    print(str(total_time_two))
+            total_distance_two = float(sum(distanceTwo))/1000
+            total_distance_two =round(total_distance_two,1)
+            total_time_two = timedelta(seconds=sum(durationTwo))
+            totalDurationTwo= sum(durationTwo)
+            print(total_distance_two,'km')
+            print(str(total_time_two))
 
-                    distanceFlex =document['Distance_flex']
-                    timeFlex = document['Time_flex']
-                    newDistanceFlex = round((total_distance_two-total_distance_one)/total_distance_one*100)
-                    newTimeFlex = round((totalDurationTwo-totalDurationOne)/totalDurationOne*100)
-                    if newDistanceFlex>distanceFlex:
-                        print("rejected")
-                        continue
-                    elif newTimeFlex>timeFlex:
-                        print("rejected")
-                        continue
+            distanceFlex =document['Distance_flex']
+            timeFlex = document['Time_flex']
+            newDistanceFlex = round((total_distance_two-total_distance_one)/total_distance_one*100)
+            newTimeFlex = round((totalDurationTwo-totalDurationOne)/totalDurationOne*100)
+            if newDistanceFlex>distanceFlex:
+                print("rejected")
+                continue
+            elif newTimeFlex>timeFlex:
+                print("rejected")
+                continue
 
-                    print(total_distance_two,'km')
-                    print(str(total_time_two))
+            print(total_distance_two,'km')
+            print(str(total_time_two))
+            #section after validation
+            totalCost += (total_distance_two - total_distance_one)*5
+            findUsers= db.users
+            getUserInfo =findUsers.find_one({'mailid':document['mailid']})
+            print(getUserInfo)
 
-                    findUsers= db.users
-                    getUserInfo =findUsers.find_one({'mailid':document['mailid']})
-                    print(getUserInfo)
+            tempdisplay={}
+            tempdisplay['name']=getUserInfo['name']
+            tempdisplay['end']=document['Start']
+            tempdisplay['start']=document['End']
+            tempdisplay['model']=getUserInfo['car_details'][0]['model']
+            tempdisplay['plate']=getUserInfo['car_details'][0]['plate']
+            tempdisplay['cost']= ceil(totalCost)
+            tempdisplay['time']=document['Time']
+            tempdisplay['seats']=document['No_of_persons']
+            #print(tempdisplay)
+            displayrides.append(tempdisplay)
 
                 #waypoints.append('Swargate')
                 #waypoints.append('Baner')
