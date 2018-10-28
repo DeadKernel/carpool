b3764c5c0f71d4805198c586ba897276964a49c3
Toshal Agrawal <toshalagrawal38@gmail.com>
2018-10-28 16:59:58 +0530

    Updated Polling

--- carpool/schedule.py
+++ carpool/schedule.py
@@ -1,6 +1,6 @@
 from carpool.db1 import connector
 import datetime
-
+from math import ceil
 def automaticDelete():
     db,conn1 = connector()
     currentTime = datetime.datetime.now()-datetime.timedelta(minutes=11)
@@ -13,14 +13,24 @@ def automaticDelete():
     print(documentstoTransact)
     if documentstoTransact :
         bookedRides = db.bookedRides
-        ridestoTransfer=[]
         for document in documentstoTransact:
+            ridestoTransfer=[]
             ridestoTransfer.extend(list(bookedRides.find({'route.mailid':document['mailid'],'route.time':document['Time']})))
-
-        bookedRides.delete_many({'route.time':{'$lt':currentTime}})
-        print(ridestoTransfer)
-        activeRides=db.activeRides
-        for ride in ridestoTransfer:
-            activeRides.insert_one({'trip':ride})
-
-        #bookedRides.insert_many(documentstoTransact);
+            if not ridestoTransfer:
+                findUsers= db.users
+                getUserInfo =findUsers.find_one({'mailid':document['mailid']})
+                tempdisplay={}
+                tempdisplay['mailid']=getUserInfo['mailid']
+                tempdisplay['name']=getUserInfo['name']
+                tempdisplay['end']=document['End']
+                tempdisplay['start']=document['Start']
+                tempdisplay['phno']=getUserInfo['phno']
+                tempdisplay['model']=getUserInfo['car_details'][0]['model']
+                tempdisplay['plate']=getUserInfo['car_details'][0]['plate']
+                tempdisplay['cost']= ceil(0)
+                tempdisplay['time']=document['Time']
+                tempdisplay['code']=document['code']
+                activeRides = db.activeRides
+                makeBooked = {'mailid':"-",'route':tempdisplay,'start':"-"}
+                activeRides.insert_one({'trip':makeBooked})
+                print(makeBooked)
